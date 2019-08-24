a5d1375a828310e9c4b33a2513bdb97bb1475636
anewlens <36173414+anewlens@users.noreply.github.com>
2019-08-24 01:38:34 -0400

    Updates

---
+++ precache-manifest.7967e87e7a7e76447980b5e50f4f663f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab2657f42d6535907b77aeb49061813a",
+    "url": "/routine/index.html"
+  },
+  {
+    "revision": "208432bf05e4299936bf",
+    "url": "/routine/static/css/main.808957b6.chunk.css"
+  },
+  {
+    "revision": "3736b62dda40b3b9caca",
+    "url": "/routine/static/js/2.2941ace8.chunk.js"
+  },
+  {
+    "revision": "208432bf05e4299936bf",
+    "url": "/routine/static/js/main.d42aadb1.chunk.js"
+  },
+  {
+    "revision": "7c89d689ae0f67d6f0e3",
+    "url": "/routine/static/js/runtime~main.a6f8cfa0.js"
+  }
+]);
